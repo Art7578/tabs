9fdeaac47fb67d8432d55109e6893f88d62dc28b
Art7578 <113849803+Art7578@users.noreply.github.com>
2024-01-25 18:38:46 +0200

    fix

--- js/accordions.js
+++ js/accordions.js
@@ -1,23 +1,48 @@
 const titles = document.querySelectorAll('.accordion_title');
 const contents = document.querySelectorAll('.accordion_content');
 
-titles.forEach(item => item.addEventListener('click', () => {
-    const activeContent = document.querySelector('#' + item.dataset.tab);
+titles.forEach(item => {
+    const span = item.querySelector('.span');
 
-    if (activeContent.classList.contains('active')) {
-        activeContent.classList.remove('active');
-        item.classList.remove('active');
-        activeContent.style.maxHeight = 0;
-    } else {
-        contents.forEach(element => {
-            element.classList.remove('active');
-            element.style.maxHeight = 0;
-        });
+    span.addEventListener('click', (event) => {
+        event.stopPropagation();
 
-        titles.forEach(element => element.classList.remove('active'));
+        const activeContent = document.querySelector('#' + item.dataset.tab);
 
-        item.classList.add('active');
-        activeContent.classList.add('active');
-        activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+        if (activeContent.classList.contains('active')) {
+            activeContent.classList.remove('active');
+            item.classList.remove('active');
+            span.classList.remove('active');
+            activeContent.style.maxHeight = 0;
+        } else {
+            contents.forEach(element => {
+                element.classList.remove('active');
+                element.style.maxHeight = 0;
+            });
+
+            titles.forEach(element => {
+                element.classList.remove('active');
+                const span = element.querySelector('.span');
+                span.classList.remove('active');
+            });
+
+            item.classList.add('active');
+            activeContent.classList.add('active');
+            span.classList.add('active');
+            activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+        }
+    });
+});
+
+const links = document.querySelectorAll('.link');
+
+// Получаем текущий путь страницы
+const currentPath = location.pathname;
+
+links.forEach((link) => {
+    // Сравниваем адрес текущей страницы с адресом ссылки
+    if (link.getAttribute('href') === currentPath) {
+        // Если ссылка соответствует текущей странице, добавляем класс active
+        link.classList.add('active');
     }
-}))
+});
